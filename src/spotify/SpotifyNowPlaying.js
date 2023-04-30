99c3efc9892d2ea2779730b37378660e02b0c623
WilsonLimSet <limsetia@usc.edu>
2023-04-29 17:29:49 -0700

    playing podcasts

--- src/spotify/SpotifyNowPlaying.js
+++ src/spotify/SpotifyNowPlaying.js
@@ -198,63 +198,39 @@ const SpotifyNowPlaying = (props) => {
 
       <br />
       <Box width="xs">
-        {loading ?
+      {
+        loading ? (
           <Stack align="center" mb={8}>
             <Spinner size="md" speed="0.6s" thickness={3} color="gray.500" />
           </Stack>
-          :
+        ) : (
           <Stack width="full" mb={result.isPlaying ? 2 : 4} spacing={3}>
             <Stack spacing={2} direction="row" align="center">
               <SpotifyLogo />
-              <Text fontWeight="semibold">{result.isPlaying ? 'Now playing' : "Not Currently Playing"}</Text>
-              {/* {result.isPlaying && <PlayingAnimation />} */}
-             
+              <Text fontWeight="semibold">
+                {result.isPlaying ? 'Now playing' : "Not Currently Playing"}
+              </Text>
             </Stack>
-            {result.isPlaying &&
-              <Box p={2} borderRadius="lg" borderWidth={1}>
-                <Stack direction="row" spacing={4} align="center">
-                  <Image
-                    alt={`${result.title} album art`}
-                    src={result.albumImageUrl}
-                    width={12}
-                    height={12}
-                    borderRadius="sm"
-                  />
-                  <Stack spacing={0} overflow="hidden">
-                    <Link href={result.songUrl} target="_blank">
-                      <Text
-                        fontWeight="semibold"
-                        width="full"
-                        isTruncated
-                        color="alph"
-                      >
-                        {result.title}
-                      </Text>
-                    </Link>
-                    <Text
-                      color="gray.500"
-                      isTruncated
-                    >
-                      {result.artist}
-                    </Text>
-                    <Text></Text>
-                  </Stack>
-                </Stack>
-              </Box>
-            }
-             <Stack spacing={2} direction="row" align="center">
-            <OuraRingLogo />
-            <Text fontWeight="semibold">
-            {sleepDate ? `${sleepDate} ${sleepDataText || 'Sleep Score Error'}` : 'Date Error'}
-            </Text>  
-            </Stack>   
+            {result.isPlaying && renderNowPlayingItem()}
+            <Stack spacing={2} direction="row" align="center">
+              <OuraRingLogo />
+              <Text fontWeight="semibold">
+                {sleepDate
+                  ? `${sleepDate} ${sleepDataText || 'Sleep Score Error'}`
+                  : 'Date Error'}
+              </Text>
+            </Stack>
           </Stack>
-        }
-      </Box>
-    </Center>
+        )
+                }
+            </Box>
+            </Center>
 
-  )
-};
+            )
+            };
+
+
+       
 
 export default SpotifyNowPlaying;
 
