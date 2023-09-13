2d77459d97d33392ee5b0de8183f9273d1f98904
Wilson Lim Setiawan <wilsonlimsetiawan@gmail.com>
2023-09-13 16:11:56 -0700

    trying to add METAR

--- src/Flying.js
+++ src/Flying.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const AviationData = () => {
   return (
-    <div style={{ backgroundColor: 'transparent', fontSize: '18px', fontWeight: 500, color: 'white', width: '400px', height: '278px' }}>
-      <a href="https://metar-taf.com/KSMO" id="metartaf-L4CZwZLO" style="font-size:18px; font-weight:500; color:#000; width:350px; height:278px; display:block">METAR Santa Monica Municipal Airport</a>
-<script async defer crossorigin="anonymous" src="https://metar-taf.com/embed-js/KSMO?bg_color=&layout=landscape&visibility=mi&qnh=inHg&rh=rh&target=ZyAXiWtm"></script>
+    <div style={{ backgroundColor: 'white', fontSize: '18px', fontWeight: 500, color: 'white', width: '400px', height: '278px' }}>
+      <iframe
+  title="METAR Santa Monica"
+  src="https://metar-taf.com/embed-js/KSMO?layout=landscape&visibility=mi&qnh=inHg&rh=rh&target=p2H3ehTx"
+  width="350"
+  height="278"
+  frameborder="0"
+></iframe>
 
+     
+      
     </div>
   );
 };
