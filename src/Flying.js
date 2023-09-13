838575d7ba4cd850365b94f3d88a84c60a9bab24
Wilson Lim Setiawan <wilsonlimsetiawan@gmail.com>
2023-09-13 16:19:28 -0700

    trying to add METAR

--- src/Flying.js
+++ src/Flying.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const AviationData = () => {
+  
+  useEffect(() => {
+    // Create the script element
+    const script = document.createElement('script');
+    
+    // Set the script's properties
+    script.async = true;
+    script.defer = true;
+    script.crossOrigin = 'anonymous';
+    script.src = 'https://metar-taf.com/embed-js/KSMO?layout=landscape&visibility=mi&qnh=inHg&rh=rh&target=4QVn8PNw';
+
+    // Append the script to the document's body
+    document.body.appendChild(script);
+    
+    // Cleanup function: remove the script when component is unmounted
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []); // The empty array means this useEffect will run once when the component mounts
+
   return (
-    <div style={{ backgroundColor: 'transparent', fontSize: '18px', fontWeight: 500, color: 'white', width: '400px', height: '278px' }}>
-      <a href="https://metar-taf.com/KSMO" id="metartaf-L4CZwZLO" style={{ display: 'block' }}>
-        METAR Santa Monica Municipal Airport
-      </a>
-      <script
-        async
-        defer
-        crossorigin="anonymous"
-        src="https://metar-taf.com/embed-js/KSMO?layout=landscape&visibility=mi&rh=rh&target=L4CZwZLO"
-      ></script>
-    </div>
+    <a href="https://metar-taf.com/KSMO" 
+       id="metartaf-4QVn8PNw" 
+       style={{ fontSize: '18px', fontWeight: '500', color: '#000', width: '350px', height: '278px', display: 'block' }}>
+      METAR Santa Monica Municipal Airport
+    </a>
   );
 };
 
