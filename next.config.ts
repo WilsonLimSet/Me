b528fb8ed10ca36aaabedd408619a5cad11b99ed
Wilson Lim Setiawan <wilsonlimsetiawan@Wilsons-MacBook-Pro.local>
2024-12-17 17:38:29 -0500

    remove redir

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,11 @@
 import type { NextConfig } from 'next';
 import createMDX from '@next/mdx';
-import postgres from 'postgres';
-
-export const sql = postgres(process.env.POSTGRES_URL!, {
-  ssl: 'allow',
-});
 
 const nextConfig: NextConfig = {
   pageExtensions: ['mdx', 'ts', 'tsx'],
   async redirects() {
-    if (!process.env.POSTGRES_URL) {
-      return [];
-    }
-
-    let redirects = await sql`
-      SELECT source, destination, permanent
-      FROM redirects;
-    `;
-
-    return redirects.map(({ source, destination, permanent }) => ({
-      source,
-      destination,
-      permanent: !!permanent,
-    }));
+    return [];  // Return empty array since we don't need database redirects
   },
-  // Note: Using the Rust compiler means we cannot use
-  // rehype or remark plugins. For my app, this is fine.
   experimental: {
     mdxRs: true,
   },
