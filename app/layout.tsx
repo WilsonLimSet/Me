6ac8a2c821173fec6dec382e69509610633dc5dc
Wilson Lim Setiawan <wilsonlimsetiawan@Wilsons-MacBook-Pro.local>
2024-12-17 17:12:04 -0500

    rebuilt with new template

---
+++ app/layout.tsx
@@ -0,0 +1,68 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ViewTransitions } from 'next-view-transitions';
+import { Analytics } from '@vercel/analytics/react';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://wilsonlimsetiawan.com'),
+  alternates: {
+    canonical: '/',
+  },
+  title: {
+    default: 'Wilson Lim Setiawan',
+    template: '%s | Wilson Lim Setiawan',
+  },
+  description: '',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ViewTransitions>
+      <html lang="en" className={`${inter.className}`}>
+        <body className="antialiased tracking-tight">
+          <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-white text-gray-900">
+            <main className="max-w-[60ch] mx-auto w-full space-y-6">
+              {children}
+            </main>
+            {/* <Footer /> */}
+            <Analytics />
+          </div>
+        </body>
+      </html>
+    </ViewTransitions>
+  );
+}
+
+// function Footer() {
+//   const links = [
+//     { name: '@leerob', url: 'https://x.com/leeerob' },
+//     { name: 'youtube', url: 'https://www.youtube.com/@leerob' },
+//     { name: 'linkedin', url: 'https://www.linkedin.com/in/leeerob' },
+//     { name: 'github', url: 'https://github.com/leerob' },
+//   ];
+
+//   return (
+//     <footer className="mt-12 text-center">
+//       <div className="flex justify-center space-x-4 tracking-tight">
+//         {links.map((link) => (
+//           <a
+//             key={link.name}
+//             href={link.url}
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             className="text-gray-400 hover:text-blue-500 transition-colors duration-200"
+//           >
+//             {link.name}
+//           </a>
+//         ))}
+//       </div>
+//     </footer>
+//   );
+// }
